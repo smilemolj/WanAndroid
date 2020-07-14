896561f681fd837407466abf875fe49857e52ce1
smilemolj <smilemolj@gmail.com>
2020-07-14 18:51:30 +0800

    ARouter组件化

---
+++ base/src/main/java/com/yuefeng/base/Base_MainActivity.kt
@@ -0,0 +1,12 @@
+package com.yuefeng.base
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Base_MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base__main)
+    }
+}
